32e8e5a5ec5bceaeb41215f277ed75140ad14885
yohan <noyohanx@naver.com>
2023-05-25 20:30:08 +0900

    :sparkles: :: movie post 기능 완성

--- movie/api/views.py
+++ movie/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import *
 
-# Create your views here.
+class MovieAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+    def patch(self, request):
+        pass
