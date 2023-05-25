c1e0d9ba9778796eb628a191551b1211dd286ed6
yohan <noyohanx@naver.com>
2023-05-25 21:07:56 +0900

    :sparkles: :: defailt view class 따로 정의

--- movie/api/views.py
+++ movie/api/views.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
 from .models import *
+from django.shortcuts import render, get_object_or_404
 
 class MovieAPIView(APIView):
-    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         serializer = MovieSerializer(data=request.data)
@@ -20,9 +20,27 @@ class MovieAPIView(APIView):
         queryset = Movie.objects.all()
         serializer = MovieSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class MovieDefailView(APIView):
+    def get_object(self, pk):
+        movie = get_object_or_404(Movie, id = pk)
+        return movie
 
-    def delete(self, request):
-        pass
+    def get(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def patch(self, request):
-        pass
+    def delete(self, request, pk):
+        queryset = Movie.objects.get(pk=pk)
+        queryset.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, pk):
+        queryset = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(queryset, data=request, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
