4b9950d9a8e0c4393c70f6be8dff5854e71d7df7
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-26 20:48:32 +0900

    :recycle: :: auth -> account 서버로 수정

---
+++ account/src/main/kotlin/com/indistraw/account/IndistrawAccountApplication.kt
@@ -0,0 +1,11 @@
+package com.indistraw.account
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IndistrawAccountApplication
+
+fun main(args: Array<String>) {
+	runApplication<IndistrawAccountApplication>(*args)
+}
