50cb24c6f6961ac0f5b5cc85c3da72eb433ca3df
yohan <noyohanx@naver.com>
2023-05-26 15:43:29 +0900

    :sparkles: :: paganation 추가

--- movie/api/views.py
+++ movie/api/views.py
@@ -1,12 +1,19 @@
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 from .serializers import *
 from .models import *
+from .pagination import PaginationHandlerMixin
 from django.shortcuts import render, get_object_or_404
 
-class MovieAPIView(APIView):
+class MoviePagination(PageNumberPagination):
+    page_size = 20
+
+class MovieAPIView(APIView, PaginationHandlerMixin):
+    pagination_class = MoviePagination
+    serializer_class = MovieSerializer
 
     def post(self, request):
         if IsAuthenticated:
@@ -19,12 +26,17 @@ class MovieAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def get(self, request):
+    def get(self, request, format=None, *args, **kwargs):
         if IsAuthenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         
         queryset = Movie.objects.all()
-        serializer = MovieSerializer(queryset, many=True)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_paginated_response(self.serializer_class(page,many=True).data)
+        else:
+            serializer = self.serializer_class(queryset, many=True)
+        
         return Response(serializer.data, status=status.HTTP_200_OK)
     
 class MovieDefailView(APIView):
@@ -34,8 +46,8 @@ class MovieDefailView(APIView):
         return movie
 
     def get(self, request, pk):
-        if IsAuthenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        # if IsAuthenticated:
+        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
         
         movie = self.get_object(pk)
         serializer = MovieSerializer(movie)
