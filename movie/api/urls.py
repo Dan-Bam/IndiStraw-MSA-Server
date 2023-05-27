3358bdb7db075da5ff5e76fbf28ba55de199542e
yohan <noyohanx@naver.com>
2023-05-27 23:53:00 +0900

    :recycle: :: modelviewset 으로 인해 url 라우팅

--- movie/api/urls.py
+++ movie/api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .views import MovieAPIView, MovieDefailView
+from django.urls import path, include
+from .views import MovieViewSet, MovieDefailView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('', MovieViewSet)  
 
 urlpatterns = [
-    path('', MovieAPIView.as_view(), name = "movie_api"),
+    path('', include(router.urls)),
     path('<int:pk>/', MovieDefailView.as_view(), name = "movie_api_detail")
 ]
