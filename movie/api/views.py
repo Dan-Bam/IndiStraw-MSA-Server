e6d938a499e6ab88b1086ddd8c8775752d4c4807
yohan <noyohanx@naver.com>
2023-05-27 23:54:31 +0900

    :sparkles: :: pagenation modelview 이용

--- movie/api/views.py
+++ movie/api/views.py
@@ -5,39 +5,18 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 from .serializers import *
 from .models import *
-from .pagination import PaginationHandlerMixin
+from .pagination import PageNumberPagination
 from django.shortcuts import render, get_object_or_404
+from rest_framework.viewsets import ModelViewSet
 
-class MoviePagination(PageNumberPagination):
-    page_size = 20
 
-class MovieAPIView(APIView, PaginationHandlerMixin):
-    pagination_class = MoviePagination
+class MovieViewSet(ModelViewSet):
+    queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    pagination_class = PageNumberPagination
 
-    def post(self, request):
-        # if IsAuthenticated:
-        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
-        
-        serializer = MovieSerializer(data=request.data)
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     
-    def get(self, request, format=None, *args, **kwargs):
-        # if IsAuthenticated:
-        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
-        
-        queryset = Movie.objects.all()
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_paginated_response(self.serializer_class(page,many=True).data)
-        else:
-            serializer = self.serializer_class(queryset, many=True)
-        
-        return Response(serializer.data, status=status.HTTP_200_OK)
     
 class MovieDefailView(APIView):
     
