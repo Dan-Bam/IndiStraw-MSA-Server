4e82ae72456cad8cdb24ec30f75d68ce476c3886
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-30 17:41:51 +0900

    :bug: :: 정규표현식 안되는 이슈 해결

--- account/src/main/kotlin/com/indistraw/account/adapter/input/request/SignUpRequest.kt
+++ account/src/main/kotlin/com/indistraw/account/adapter/input/request/SignUpRequest.kt
@@ -1,7 +1,6 @@
 package com.indistraw.account.adapter.input.request
 
 import org.hibernate.validator.constraints.Length
-import javax.validation.constraints.Max
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
@@ -13,7 +12,7 @@ data class SignUpRequest(
 
     @field:NotNull
     @field:Length(min = 8, max = 20)
-    @field:Pattern(regexp = "[0-9a-zA-Z]{0,20}")
+    @field:Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]+$")
     val password: String,
 
     @field:NotNull
