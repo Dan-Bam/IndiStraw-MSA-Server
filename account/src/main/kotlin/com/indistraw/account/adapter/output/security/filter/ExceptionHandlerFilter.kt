a73a206159bf925f4658f52d20e59c2578d7e1e6
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-30 14:09:54 +0900

    :sparkles: :: exceptionHandlerFilter 추가

---
+++ account/src/main/kotlin/com/indistraw/account/adapter/output/security/filter/ExceptionHandlerFilter.kt
@@ -0,0 +1,41 @@
+package com.indistraw.account.adapter.output.security.filter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.indistraw.account.application.common.error.ErrorCode
+import com.indistraw.account.application.common.error.exception.IndiStrawAccountException
+import com.indistraw.account.application.common.error.response.ErrorResponse
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.JwtException
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class ExceptionHandlerFilter: OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        runCatching {
+            filterChain.doFilter(request, response)
+        }.onFailure { exception ->
+            when(exception) {
+                is ExpiredJwtException -> exceptionToResponse(ErrorCode.EXPIRED_ACCESS_TOKEN, response)
+                is JwtException -> exceptionToResponse(ErrorCode.INVALID_TOKEN, response)
+                is IndiStrawAccountException -> exceptionToResponse(exception.errorCode, response)
+            }
+        }
+    }
+
+    private fun exceptionToResponse(errorCode: ErrorCode, response: HttpServletResponse) {
+        response.status = errorCode.status
+        response.contentType = "application/json"
+        response.characterEncoding = "utf-8"
+        val errorResponse = ErrorResponse(errorCode.message, errorCode.status)
+        val errorResponseToJson = ObjectMapper().writeValueAsString(errorResponse)
+        response.writer.write(errorResponseToJson)
+    }
+
+}
