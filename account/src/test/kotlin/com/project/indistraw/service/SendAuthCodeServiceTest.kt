dde2102eed2167b851b889782f980102841e2a8f
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-02 00:31:12 +0900

    :sparkles: :: sendAuthCode testCode 추가

---
+++ account/src/test/kotlin/com/project/indistraw/service/SendAuthCodeServiceTest.kt
@@ -0,0 +1,36 @@
+package com.project.indistraw.service
+
+import com.project.indistraw.account.application.port.output.CommandAuthCodePort
+import com.project.indistraw.account.application.port.output.SendMessagePort
+import com.project.indistraw.account.application.service.SendAuthCodeService
+import com.project.indistraw.global.event.CreateAuthenticationEvent
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.springframework.context.ApplicationEventPublisher
+
+class SendAuthCodeServiceTest: BehaviorSpec({
+    val sendMessagePort = mockk<SendMessagePort>()
+    val commandAuthCodePort = mockk<CommandAuthCodePort>()
+    val publisher = mockk<ApplicationEventPublisher>()
+    val sendAuthCodeService = SendAuthCodeService(sendMessagePort, commandAuthCodePort, publisher)
+
+    Given("핸드폰 번호가 주어질때") {
+        val phoneNumber = "01012345678"
+        val code = 1234
+        val createAuthenticationEvent = CreateAuthenticationEvent(phoneNumber)
+
+        every { sendMessagePort.sendMessage(any(), any()) } returns Unit
+        every { publisher.publishEvent(createAuthenticationEvent) } returns Unit
+        every { commandAuthCodePort.saveAuthCode(any()) } returns code
+
+        When("authCode 발송 요청을 하면") {
+            sendAuthCodeService.execute(phoneNumber)
+
+            Then("authCode가 발송이 되어야 한다.") {
+                verify { sendMessagePort.sendMessage(any(), any()) }
+            }
+        }
+    }
+})
