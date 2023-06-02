55694e9adbbc84694e2e9541331bddb4d49ab093
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-02 12:29:55 +0900

    :sparkles: :: findByIdxOrNull 메서드 추가

--- account/src/main/kotlin/com/project/indistraw/account/application/port/output/QueryAccountPort.kt
+++ account/src/main/kotlin/com/project/indistraw/account/application/port/output/QueryAccountPort.kt
@@ -1,11 +1,13 @@
 package com.project.indistraw.account.application.port.output
 
 import com.project.indistraw.account.domain.Account
+import java.util.UUID
 
 interface QueryAccountPort {
 
     fun existsById(id: String): Boolean
     fun existsByPhoneNumber(phoneNumber: String): Boolean
     fun findByIdOrNull(id: String): Account?
+    fun findByIdxOrNull(idx: UUID): Account?
 
 }
