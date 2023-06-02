b32d722c67759cad71c2abea14189184629cadd1
yohan <noyohanx@naver.com>
2023-06-02 20:03:25 +0900

    :sparkles: :: upload view 완성

--- file/api/views.py
+++ file/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from .serializers import UploadSerializer
 
-# Create your views here.
+class UploadViewset(ViewSet):
+    serializer_class = UploadSerializer
+
+    def list(self, request):
+        return Response("GET API")
+    
+    def create(self, request):
+        file_uploaded = request.FILES.get('file_uploaded')
+        content_type = file_uploaded.content_type
+        response = "POST API and you have uploaded a {} file".format(content_type)
+        return Response(response)
