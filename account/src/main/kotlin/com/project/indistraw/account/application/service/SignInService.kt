9953a2266056945e51a6a8998e1672d207943aee
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-02 12:25:24 +0900

    :sparkles: :: refreshToken save event 생성

--- account/src/main/kotlin/com/project/indistraw/account/application/service/SignInService.kt
+++ account/src/main/kotlin/com/project/indistraw/account/application/service/SignInService.kt
@@ -1,28 +1,49 @@
 package com.project.indistraw.account.application.service
 
-import com.project.indistraw.account.application.common.annotation.ServiceWithReadOnlyTransaction
+import com.project.indistraw.account.application.common.annotation.ServiceWithTransaction
 import com.project.indistraw.account.application.exception.AccountNotFoundException
 import com.project.indistraw.account.application.exception.PasswordNotMatchException
 import com.project.indistraw.account.application.port.input.SignInUseCase
 import com.project.indistraw.account.application.port.input.dto.SignInDto
-import com.project.indistraw.account.application.port.output.JwtGeneratePort
 import com.project.indistraw.account.application.port.output.PasswordEncodePort
 import com.project.indistraw.account.application.port.output.QueryAccountPort
+import com.project.indistraw.account.application.port.output.TokenGeneratePort
 import com.project.indistraw.account.application.port.output.dto.TokenDto
+import com.project.indistraw.account.domain.Account
+import com.project.indistraw.global.event.SaveRefreshTokenEvent
+import org.springframework.context.ApplicationEventPublisher
 
-@ServiceWithReadOnlyTransaction
+@ServiceWithTransaction
 class SignInService(
     private val queryAccountPort: QueryAccountPort,
     private val passwordEncodePort: PasswordEncodePort,
-    private val jwtGeneratePort: JwtGeneratePort
+    private val tokenGeneratePort: TokenGeneratePort,
+    private val publisher: ApplicationEventPublisher
 ): SignInUseCase {
 
     override fun execute(dto: SignInDto): TokenDto {
-        val account = queryAccountPort.findByIdOrNull(dto.id) ?: throw AccountNotFoundException()
+        val account = queryAccountPort.findByIdOrNull(dto.id)
+            ?: throw AccountNotFoundException()
+
         if (!passwordEncodePort.isPasswordMatch(dto.password, account.encodedPassword)) {
             throw PasswordNotMatchException()
         }
-        return jwtGeneratePort.generateToken(account.accountIdx, account.authority)
+
+        val token = tokenGeneratePort.generateToken(account.accountIdx, account.authority)
+
+        // refreshToken을 redis에 저장하기 위한 event를 publish합니다.
+        publishSaveRefreshToken(token, account)
+
+        return token
+    }
+
+    private fun publishSaveRefreshToken(token: TokenDto, account: Account) {
+        val saveRefreshTokenEvent = SaveRefreshTokenEvent(
+            token.refreshToken,
+            account.accountIdx,
+            token.refreshTokenExpiredAt
+        )
+        publisher.publishEvent(saveRefreshTokenEvent)
     }
 
 }
--- account/src/test/kotlin/com/project/indistraw/service/SignInServiceTest.kt
+++ account/src/test/kotlin/com/project/indistraw/service/SignInServiceTest.kt
@@ -3,41 +3,51 @@ package com.project.indistraw.service
 import com.project.indistraw.account.application.exception.AccountNotFoundException
 import com.project.indistraw.account.application.exception.PasswordNotMatchException
 import com.project.indistraw.account.application.port.input.dto.SignInDto
-import com.project.indistraw.account.application.port.output.JwtGeneratePort
+import com.project.indistraw.account.application.port.output.TokenGeneratePort
 import com.project.indistraw.account.application.port.output.PasswordEncodePort
 import com.project.indistraw.account.application.port.output.QueryAccountPort
 import com.project.indistraw.account.application.port.output.dto.TokenDto
 import com.project.indistraw.account.application.service.SignInService
 import com.project.indistraw.common.AnyValueObjectGenerator
 import com.project.indistraw.account.domain.Account
+import com.project.indistraw.global.event.SaveRefreshTokenEvent
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.springframework.context.ApplicationEventPublisher
 import java.util.*
 
 class SignInServiceTest: BehaviorSpec({
     val queryAccountPort = mockk<QueryAccountPort>()
     val passwordEncodePort = mockk<PasswordEncodePort>()
-    val jwtGeneratePort = mockk<JwtGeneratePort>()
-    val signInService = SignInService(queryAccountPort, passwordEncodePort, jwtGeneratePort)
+    val tokenGeneratePort = mockk<TokenGeneratePort>()
+    val publisher = mockk<ApplicationEventPublisher>(relaxed = true)
+    val signInService = SignInService(queryAccountPort, passwordEncodePort, tokenGeneratePort, publisher)
 
     Given("signInDto가 주어질때") {
         val signInDto = AnyValueObjectGenerator.anyValueObject<SignInDto>("id" to "mock id")
         val account = AnyValueObjectGenerator.anyValueObject<Account>("accountIdx" to UUID.randomUUID())
-        val tokenDto = AnyValueObjectGenerator.anyValueObject<TokenDto>("accessToken" to "mock accessToken")
+        val refreshToken = "test refreshToken"
+        val tokenDto = AnyValueObjectGenerator.anyValueObject<TokenDto>("refreshToken" to refreshToken)
+        val saveRefreshTokenEvent = SaveRefreshTokenEvent(refreshToken, account.accountIdx, 0)
 
         every { queryAccountPort.findByIdOrNull(signInDto.id) } returns account
         every { passwordEncodePort.isPasswordMatch(signInDto.password, account.encodedPassword) } returns true
-        every { jwtGeneratePort.generateToken(account.accountIdx, account.authority) } returns tokenDto
+        every { publisher.publishEvent(saveRefreshTokenEvent) } returns  Unit
+        every { tokenGeneratePort.generateToken(account.accountIdx, account.authority) } returns tokenDto
 
         When("로그인을 요청하면") {
             val result = signInService.execute(signInDto)
 
             Then("토큰이 생성 되어야 한다.") {
-                verify { jwtGeneratePort.generateToken(account.accountIdx, account.authority) }
+                verify { tokenGeneratePort.generateToken(account.accountIdx, account.authority) }
+            }
+
+            Then("refreshToken save 이벤트가 발생해야한다.") {
+                verify { publisher.publishEvent(saveRefreshTokenEvent) }
             }
 
             Then("result와 tokenDto는 같아야 한다.") {
@@ -48,7 +58,7 @@ class SignInServiceTest: BehaviorSpec({
         When("없는 id로 요청하면") {
             every { queryAccountPort.findByIdOrNull(signInDto.id) } returns null
 
-            Then("AccountNotFount 예외가 터져야 한다.") {
+            Then("AccountNotFound가 터져야 한다.") {
                 shouldThrow<AccountNotFoundException> {
                     signInService.execute(signInDto)
                 }
@@ -59,7 +69,7 @@ class SignInServiceTest: BehaviorSpec({
             every { queryAccountPort.findByIdOrNull(signInDto.id) } returns account
             every { passwordEncodePort.isPasswordMatch(signInDto.password, account.encodedPassword) } returns false
 
-            Then("PasswordNotMatchException 예외가 터져야 한다.") {
+            Then("PasswordNotMatchException이 터져야 한다.") {
                 shouldThrow<PasswordNotMatchException> {
                     signInService.execute(signInDto)
                 }
