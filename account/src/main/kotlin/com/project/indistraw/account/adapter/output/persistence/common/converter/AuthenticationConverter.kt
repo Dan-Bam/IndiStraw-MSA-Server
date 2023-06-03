c5da55abbdeedfc8444ba9c823ffc9f5f47e4181
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-04 00:08:37 +0900

    :sparkles: :: authentication converter 추가

---
+++ account/src/main/kotlin/com/project/indistraw/account/adapter/output/persistence/common/converter/AuthenticationConverter.kt
@@ -0,0 +1,30 @@
+package com.project.indistraw.account.adapter.output.persistence.common.converter
+
+import com.project.indistraw.account.adapter.output.persistence.entity.AuthenticationEntity
+import com.project.indistraw.account.domain.Authentication
+import org.springframework.stereotype.Component
+
+@Component
+class AuthenticationConverter {
+
+    infix fun toEntity(domain: Authentication): AuthenticationEntity =
+        domain.let {
+            AuthenticationEntity(
+                phoneNumber = it.phoneNumber,
+                attemptCount = it.attemptCount,
+                isVerified = it.isVerified,
+                expiredAt = it.expiredAt
+            )
+        }
+
+    infix fun toDomain(entity: AuthenticationEntity?): Authentication? =
+        entity?.let {
+            Authentication(
+                phoneNumber = it.phoneNumber,
+                attemptCount = it.attemptCount,
+                isVerified = it.isVerified,
+                expiredAt = it.expiredAt
+            )
+        }
+
+}
