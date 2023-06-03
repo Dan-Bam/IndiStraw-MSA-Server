e2b7b8381d47019ec9cd6e1e1a03817411ea1a67
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-03 23:21:49 +0900

    :sparkles: :: phoneNumber index로 지정

--- account/src/main/kotlin/com/project/indistraw/account/adapter/output/persistence/entity/AuthCodeEntity.kt
+++ account/src/main/kotlin/com/project/indistraw/account/adapter/output/persistence/entity/AuthCodeEntity.kt
@@ -2,14 +2,17 @@ package com.project.indistraw.account.adapter.output.persistence.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
-import org.springframework.data.redis.core.index.Indexed
+import org.springframework.data.redis.core.TimeToLive
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 @RedisHash("auth_code")
 data class AuthCodeEntity(
     @Id
+    val phoneNumber: String,
+
     val authCode: Int,
 
-    @Indexed
-    val phoneNumber: String
+    @TimeToLive(unit = TimeUnit.SECONDS)
+    val expiredAt: Long
 )
