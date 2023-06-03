0124da25792ad4b2ef90e978915a005ac1c0a7e6
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-04 00:04:48 +0900

    :recycle: :: UUID -> String으로 변경

--- account/src/main/kotlin/com/project/indistraw/account/adapter/output/persistence/repository/AuthCodeRepository.kt
+++ account/src/main/kotlin/com/project/indistraw/account/adapter/output/persistence/repository/AuthCodeRepository.kt
@@ -2,6 +2,5 @@ package com.project.indistraw.account.adapter.output.persistence.repository
 
 import com.project.indistraw.account.adapter.output.persistence.entity.AuthCodeEntity
 import org.springframework.data.repository.CrudRepository
-import java.util.*
 
-interface AuthCodeRepository: CrudRepository<AuthCodeEntity, UUID>
+interface AuthCodeRepository: CrudRepository<AuthCodeEntity, String>
