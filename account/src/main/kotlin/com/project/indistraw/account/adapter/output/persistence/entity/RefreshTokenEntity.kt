ebf978d56cb79560c2a02642ac9ccbb177285f40
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-04 00:06:37 +0900

    :recycle: :: expiredAt 타입 Int -> Long으로 변경

--- account/src/main/kotlin/com/project/indistraw/account/adapter/output/persistence/entity/RefreshTokenEntity.kt
+++ account/src/main/kotlin/com/project/indistraw/account/adapter/output/persistence/entity/RefreshTokenEntity.kt
@@ -3,8 +3,7 @@ package com.project.indistraw.account.adapter.output.persistence.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
-import org.springframework.data.redis.core.index.Indexed
-import java.util.UUID
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 @RedisHash("refresh_token")
@@ -12,9 +11,8 @@ data class RefreshTokenEntity(
     @Id
     val refreshToken: String,
 
-    @Indexed
     val accountIdx: UUID,
 
     @TimeToLive(unit = TimeUnit.SECONDS)
-    val expiredAt: Int
+    val expiredAt: Long
 )
