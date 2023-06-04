146111e069975794a280a4402c5e306ca36a4d03
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-04 21:48:47 +0900

    :sparkles: :: 프로필 수정 기능 추가

--- account/src/main/kotlin/com/project/indistraw/account/adapter/input/AccountWebAdapter.kt
+++ account/src/main/kotlin/com/project/indistraw/account/adapter/input/AccountWebAdapter.kt
@@ -1,8 +1,10 @@
 package com.project.indistraw.account.adapter.input
 
 import com.project.indistraw.account.adapter.input.converter.AccountDataConverter
+import com.project.indistraw.account.adapter.input.request.UpdateAccountProfileRequest
 import com.project.indistraw.account.adapter.input.request.UpdatePasswordRequest
 import com.project.indistraw.account.application.port.input.FindAccountIdUseCase
+import com.project.indistraw.account.application.port.input.UpdateAccountProfileUseCase
 import com.project.indistraw.account.application.port.input.UpdatePasswordUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -13,7 +15,8 @@ import org.springframework.web.bind.annotation.*
 class AccountWebAdapter(
     private val accountDataConverter: AccountDataConverter,
     private val findAccountIdUseCase: FindAccountIdUseCase,
-    private val updatePasswordUseCase: UpdatePasswordUseCase
+    private val updatePasswordUseCase: UpdatePasswordUseCase,
+    private val updateAccountProfileUseCase: UpdateAccountProfileUseCase
 ) {
 
     @GetMapping("/phone-number/{phoneNumber}")
@@ -21,9 +24,14 @@ class AccountWebAdapter(
         findAccountIdUseCase.execute(phoneNumber)
             .let { ResponseEntity.ok(mapOf("id" to it)) }
 
-    @PatchMapping("/change/password")
+    @PatchMapping("/update/password")
     fun sendAuthCode(@RequestBody request: UpdatePasswordRequest): ResponseEntity<Void> =
         updatePasswordUseCase.execute(accountDataConverter toDto request)
+            .run { ResponseEntity.status(HttpStatus.RESET_CONTENT).build() }
+
+    @PatchMapping("/update/profile")
+    fun sendAuthCode(@RequestBody request: UpdateAccountProfileRequest): ResponseEntity<Void> =
+        updateAccountProfileUseCase.execute(accountDataConverter toDto request)
             .run { ResponseEntity.status(HttpStatus.RESET_CONTENT).build() }
 
 }
