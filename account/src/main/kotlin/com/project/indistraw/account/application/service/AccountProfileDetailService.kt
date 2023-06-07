7d2b5c87bc6a5efb89a685e9e61f63fabc66a671
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-07 14:14:55 +0900

    :sparkles: :: 프로필 조회 기능 추가

---
+++ account/src/main/kotlin/com/project/indistraw/account/application/service/AccountProfileDetailService.kt
@@ -0,0 +1,27 @@
+package com.project.indistraw.account.application.service
+
+import com.project.indistraw.account.application.common.annotation.ServiceWithReadOnlyTransaction
+import com.project.indistraw.account.application.exception.AccountNotFoundException
+import com.project.indistraw.account.application.port.input.AccountProfileDetailUseCase
+import com.project.indistraw.account.application.port.input.dto.AccountProfileDetailDto
+import com.project.indistraw.account.application.port.output.AccountSecurityPort
+import com.project.indistraw.account.application.port.output.QueryAccountPort
+
+@ServiceWithReadOnlyTransaction
+class AccountProfileDetailService(
+    private val queryAccountPort: QueryAccountPort,
+    private val accountSecurityPort: AccountSecurityPort
+): AccountProfileDetailUseCase {
+
+    override fun execute(): AccountProfileDetailDto {
+        val accountIdx = accountSecurityPort.getCurrentAccountIdx()
+        val account = queryAccountPort.findByIdxOrNull(accountIdx)
+            ?: throw AccountNotFoundException()
+        return AccountProfileDetailDto(
+            id = account.id,
+            name = account.name,
+            profileUrl = account.profileUrl
+        )
+    }
+
+}
