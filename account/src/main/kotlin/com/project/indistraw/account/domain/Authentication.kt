8b4027de788592457e50c76f1fb5be370f830c8d
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-11 14:38:23 +0900

    :recycle: :: 문자 요청과 인증코드 확인 요청 count 변수 구분

--- account/src/main/kotlin/com/project/indistraw/account/domain/Authentication.kt
+++ account/src/main/kotlin/com/project/indistraw/account/domain/Authentication.kt
@@ -2,37 +2,37 @@ package com.project.indistraw.account.domain
 
 data class Authentication(
     val phoneNumber: String,
-    val attemptCount: Long,
+    val authCodeCount: Long,
+    val authenticationCount: Long,
     val isVerified: Boolean,
     val expiredAt: Long
 ) {
 
     constructor(phoneNumber: String): this(
         phoneNumber = phoneNumber,
-        attemptCount = 0,
+        authCodeCount = 0,
+        authenticationCount = 0,
         isVerified = false,
         expiredAt = EXPIRED_AT
     )
 
     companion object {
-        const val MAX_ATTEMPT_COUNT = 5L
         const val EXPIRED_AT = 7200L
     }
 
-    fun increaseCount(): Authentication =
+    fun increaseAuthCodeCount(): Authentication =
         this.copy(
-            phoneNumber = phoneNumber,
-            attemptCount = attemptCount.inc(),
-            isVerified = isVerified,
-            expiredAt = expiredAt
+            authCodeCount = authCodeCount.inc()
+        )
+
+    fun increaseAuthenticationCount(): Authentication =
+        this.copy(
+            authenticationCount = authenticationCount.inc()
         )
 
     fun certified(): Authentication =
         this.copy(
-            phoneNumber = phoneNumber,
-            attemptCount = MAX_ATTEMPT_COUNT,
             isVerified = true,
-            expiredAt = expiredAt
         )
 
 }
