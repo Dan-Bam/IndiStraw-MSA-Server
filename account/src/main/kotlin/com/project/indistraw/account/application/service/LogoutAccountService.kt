df7dd1d191f7742e26d7b0ae0ecae4e28494d198
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-16 16:17:29 +0900

    :sparkles: :: 유효하지 않은 토큰 타입 예외처리 추가

--- account/src/main/kotlin/com/project/indistraw/account/application/service/LogoutAccountService.kt
+++ account/src/main/kotlin/com/project/indistraw/account/application/service/LogoutAccountService.kt
@@ -4,16 +4,22 @@ import com.project.indistraw.account.application.common.annotation.ServiceWithTr
 import com.project.indistraw.account.application.port.input.LogoutAccountUseCase
 import com.project.indistraw.account.application.port.output.CommandRefreshTokenPort
 import com.project.indistraw.account.application.port.output.QueryRefreshTokenPort
+import com.project.indistraw.account.application.port.output.TokenParsePort
 import com.project.indistraw.global.security.token.common.exception.ExpiredRefreshTokenException
+import com.project.indistraw.global.security.token.common.exception.InvalidTokenTypeException
 
 @ServiceWithTransaction
 class LogoutAccountService(
     private val commandRefreshTokenPort: CommandRefreshTokenPort,
+    private val tokenParsePort: TokenParsePort,
     private val queryRefreshTokenPort: QueryRefreshTokenPort,
 ): LogoutAccountUseCase {
 
     override fun execute(refreshToken: String) {
-        val refreshTokenDomain = queryRefreshTokenPort.findByRefreshTokenOrNull(refreshToken)
+        val parsedRefreshToken = tokenParsePort.parseRefreshTokenToken(refreshToken)
+            ?: throw InvalidTokenTypeException()
+
+        val refreshTokenDomain = queryRefreshTokenPort.findByRefreshTokenOrNull(parsedRefreshToken)
             ?: throw ExpiredRefreshTokenException()
 
         commandRefreshTokenPort.deleteRefreshToken(refreshTokenDomain)
