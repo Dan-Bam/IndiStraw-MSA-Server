ae9c84e71be0ee2ab3a4717104f00002ca0991c0
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-19 17:58:59 +0900

    :recycle: :: address nullable 수정

--- account/src/main/kotlin/com/project/indistraw/account/adapter/input/request/AddressRequest.kt
+++ account/src/main/kotlin/com/project/indistraw/account/adapter/input/request/AddressRequest.kt
@@ -1,7 +1,7 @@
 package com.project.indistraw.account.adapter.input.request
 
 data class AddressRequest(
-    val zipcode: String?,
-    val streetAddress: String?,
-    val detailAddress: String?
+    val zipcode: String,
+    val streetAddress: String,
+    val detailAddress: String
 )
