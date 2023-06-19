2ab352564cbbebe41db6dd76cd4a32fd7e040f49
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-19 17:14:24 +0900

    :recycle: :: address nullable 수정

--- account/src/main/kotlin/com/project/indistraw/account/application/port/input/dto/AddressDto.kt
+++ account/src/main/kotlin/com/project/indistraw/account/application/port/input/dto/AddressDto.kt
@@ -1,7 +1,7 @@
 package com.project.indistraw.account.application.port.input.dto
 
 data class AddressDto(
-    val zipcode: String,
-    val streetAddress: String,
-    val detailAddress: String
+    val zipcode: String?,
+    val streetAddress: String?,
+    val detailAddress: String?
 )
