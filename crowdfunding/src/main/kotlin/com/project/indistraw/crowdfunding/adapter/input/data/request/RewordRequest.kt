139d8be8a9cf1815ca869f8521fec3aca53869c7
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-21 10:41:23 +0900

    :sparkles: :: rewordDto 추가

---
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/input/data/request/RewordRequest.kt
@@ -0,0 +1,17 @@
+package com.project.indistraw.crowdfunding.adapter.input.data.request
+
+import javax.validation.constraints.NotNull
+
+data class RewordRequest(
+    @field:NotNull
+    val title: String,
+
+    @field:NotNull
+    val description: String,
+
+    @field:NotNull
+    val price: Long,
+
+    @field:NotNull
+    val thumbnailUrl: String
+)
