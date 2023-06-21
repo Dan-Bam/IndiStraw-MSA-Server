aed65c7d57d0c906a5ae160d419020b8a36b8710
yohan <noyohanx@naver.com>
2023-06-21 10:31:29 +0900

    :sparkles: :: 검색 기능 생성

--- search/api/views.py
+++ search/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.filters import SearchFilter
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import TestSeriaizer
+from .models import Test
+from .pagination import PageNumberPagination
+
+class SearchViewSet(viewsets.ModelViewSet):
+    queryset = Test.objects.all()
+    serializer_class = TestSeriaizer
+
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'description',)
