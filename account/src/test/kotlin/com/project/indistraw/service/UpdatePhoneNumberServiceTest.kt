56c7dacac43bb16f8684827fa380220c226b2468
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-21 11:31:18 +0900

    :bug: :: 테스트 코드 수정

--- account/src/test/kotlin/com/project/indistraw/service/UpdatePhoneNumberServiceTest.kt
+++ account/src/test/kotlin/com/project/indistraw/service/UpdatePhoneNumberServiceTest.kt
@@ -2,6 +2,7 @@ package com.project.indistraw.service
 
 import com.project.indistraw.account.application.common.util.AuthenticationValidator
 import com.project.indistraw.account.application.exception.AccountNotFoundException
+import com.project.indistraw.account.application.port.output.AccountSecurityPort
 import com.project.indistraw.account.application.port.output.CommandAccountPort
 import com.project.indistraw.account.application.port.output.QueryAccountPort
 import com.project.indistraw.account.application.service.UpdatePhoneNumberService
@@ -15,24 +16,28 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.springframework.context.ApplicationEventPublisher
+import java.util.UUID
 
 class UpdatePhoneNumberServiceTest: BehaviorSpec({
     val commandAccountPort = mockk<CommandAccountPort>()
     val queryAccountPort = mockk<QueryAccountPort>()
     val authenticationValidator = mockk<AuthenticationValidator>()
     val publisher = mockk<ApplicationEventPublisher>()
-    val updatePhoneNumberService = UpdatePhoneNumberService(commandAccountPort, queryAccountPort, authenticationValidator, publisher)
+    val accountSecurityPort = mockk<AccountSecurityPort>()
+    val updatePhoneNumberService = UpdatePhoneNumberService(commandAccountPort, queryAccountPort, authenticationValidator, accountSecurityPort, publisher)
 
     Given("phoneNumber가 주어질때") {
         val phoneNumber = "01012345678"
+        val accountIdx = UUID.randomUUID()
         val account = AnyValueObjectGenerator.anyValueObject<Account>("phoneNumber" to phoneNumber)
         val authentication = AnyValueObjectGenerator.anyValueObject<Authentication>("phoneNumber" to phoneNumber)
         val deleteAuthenticationEvent = DeleteAuthenticationEvent(authentication)
 
-        every { queryAccountPort.findByPhoneNumberOrNull(phoneNumber) } returns account
+        every { queryAccountPort.findByIdxOrNull(accountIdx) } returns account
         every { authenticationValidator.verifyAuthenticationByPhoneNumber(account.phoneNumber) } returns authentication
         every { publisher.publishEvent(deleteAuthenticationEvent) } returns Unit
         every { commandAccountPort.saveAccount(account.copy(phoneNumber = phoneNumber)) } returns account.accountIdx
+        every { accountSecurityPort.getCurrentAccountIdx() } returns accountIdx
 
         When("전화번호 수정 요청을 하면") {
             updatePhoneNumberService.execute(phoneNumber)
@@ -42,8 +47,8 @@ class UpdatePhoneNumberServiceTest: BehaviorSpec({
             }
         }
 
-        When("없는 phoneNumber로 요청하면") {
-            every { queryAccountPort.findByPhoneNumberOrNull(phoneNumber) } returns null
+        When("없는 accountIdx 요청하면") {
+            every { queryAccountPort.findByIdxOrNull(accountIdx) } returns null
 
             Then("AccountNotFound가 터져야 한다.") {
                 shouldThrow<AccountNotFoundException> {
