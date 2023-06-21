a7bbf718cd4b59a283780fe47b8b48d094c8d6d6
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-21 11:26:58 +0900

    :bug: :: 수정 안되는 이슈 해결

--- account/src/main/kotlin/com/project/indistraw/account/application/service/UpdatePhoneNumberService.kt
+++ account/src/main/kotlin/com/project/indistraw/account/application/service/UpdatePhoneNumberService.kt
@@ -3,6 +3,7 @@ package com.project.indistraw.account.application.service
 import com.project.indistraw.account.application.common.annotation.ServiceWithTransaction
 import com.project.indistraw.account.application.common.util.AuthenticationValidator
 import com.project.indistraw.account.application.exception.AccountNotFoundException
+import com.project.indistraw.account.application.port.output.AccountSecurityPort
 import com.project.indistraw.account.application.port.output.CommandAccountPort
 import com.project.indistraw.account.application.port.output.QueryAccountPort
 import com.project.indistraw.account.application.port.output.UpdatePhoneNumberUseCase
@@ -14,14 +15,16 @@ class UpdatePhoneNumberService(
     private val commandAccountPort: CommandAccountPort,
     private val queryAccountPort: QueryAccountPort,
     private val authenticationValidator: AuthenticationValidator,
+    private val accountSecurityPort: AccountSecurityPort,
     private val publisher: ApplicationEventPublisher
 ): UpdatePhoneNumberUseCase {
 
     override fun execute(phoneNumber: String) {
-        val account = queryAccountPort.findByPhoneNumberOrNull(phoneNumber)
+        val accountIdx = accountSecurityPort.getCurrentAccountIdx()
+        val account = queryAccountPort.findByIdxOrNull(accountIdx)
             ?: throw AccountNotFoundException()
 
-        val authentication = authenticationValidator.verifyAuthenticationByPhoneNumber(account.phoneNumber)
+        val authentication = authenticationValidator.verifyAuthenticationByPhoneNumber(phoneNumber)
         publisher.publishEvent(DeleteAuthenticationEvent(authentication))
 
         commandAccountPort.saveAccount(account.copy(phoneNumber = phoneNumber))
