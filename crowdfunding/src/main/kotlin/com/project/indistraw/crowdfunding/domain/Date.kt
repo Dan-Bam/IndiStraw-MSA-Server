48b9ae0264d652dd0f9214e2534285f1a2b02dbc
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-22 09:15:51 +0900

    :sparkles: :: Aggregate 커스텀 어노테이션 추가

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/domain/Date.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/domain/Date.kt
@@ -1,8 +1,10 @@
 package com.project.indistraw.crowdfunding.domain
 
+import com.project.indistraw.crowdfunding.application.common.annotation.Aggregate
 import java.time.LocalDate
 import java.time.LocalDateTime
 
+@Aggregate
 data class Date(
     val createdDate: LocalDateTime,
     val endDate: LocalDate
--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/domain/DirectorAccount.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/domain/DirectorAccount.kt
@@ -1,5 +1,8 @@
 package com.project.indistraw.crowdfunding.domain
 
+import com.project.indistraw.crowdfunding.application.common.annotation.Aggregate
+
+@Aggregate
 data class DirectorAccount(
     val bank: String,
     val account: String
