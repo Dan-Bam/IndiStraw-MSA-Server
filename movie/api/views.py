45eac01ce45ca6e1eee1fcb25a888d5c26353ef5
yohan <noyohanx@naver.com>
2023-06-23 10:06:42 +0900

    :sparkles: :: 무비 서치 기능 추가

--- movie/api/views.py
+++ movie/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
@@ -14,6 +15,10 @@ class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     pagination_class = PageNumberPagination
+
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'description',)
+
 
 
     
