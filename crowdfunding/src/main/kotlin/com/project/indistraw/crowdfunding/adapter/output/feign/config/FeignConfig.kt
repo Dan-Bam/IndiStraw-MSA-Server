3d458290eb6ef9adf453293c3f672f25b842f3e9
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-28 08:29:42 +0900

    :sparkles: :: EnableFeignClients 어노테이션 추가

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/output/feign/config/FeignConfig.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/output/feign/config/FeignConfig.kt
@@ -3,9 +3,11 @@ package com.project.indistraw.crowdfunding.adapter.output.feign.config
 import com.project.indistraw.crowdfunding.adapter.output.feign.error.FeignClientErrorDecoder
 import feign.codec.ErrorDecoder
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+@EnableFeignClients(basePackages = ["com.project.indistraw"])
 @Configuration
 class FeignConfig {
 
