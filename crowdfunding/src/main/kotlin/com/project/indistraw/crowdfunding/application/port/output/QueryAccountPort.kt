ef51f08ffd6eb68eff2439c3d8265bab1867ef40
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-28 09:18:34 +0900

    :fire: :: accountIdx 매개변수 삭제

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/port/output/QueryAccountPort.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/port/output/QueryAccountPort.kt
@@ -1,10 +1,9 @@
 package com.project.indistraw.crowdfunding.application.port.output
 
 import com.project.indistraw.crowdfunding.adapter.output.feign.client.data.AccountInfoDto
-import java.util.UUID
 
 interface QueryAccountPort {
 
-    fun getAccountInfo(accountIdx: UUID): AccountInfoDto
+    fun getAccountInfo(): AccountInfoDto
 
 }
