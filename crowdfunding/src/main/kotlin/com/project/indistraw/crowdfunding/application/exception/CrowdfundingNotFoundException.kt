88a23d75739dfa4ed6c41fd926077ae2d725bc0d
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-30 02:14:50 +0900

    :sparkles: :: Exception 키워드 추가

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/exception/CrowdfundingNotFoundException.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/exception/CrowdfundingNotFoundException.kt
@@ -3,4 +3,4 @@ package com.project.indistraw.crowdfunding.application.exception
 import com.project.indistraw.crowdfunding.application.common.error.ErrorCode
 import com.project.indistraw.crowdfunding.application.common.error.exception.IndiStrawCrowdfundingException
 
-class CrowdfundingNotFound: IndiStrawCrowdfundingException(ErrorCode.CROWDFUNDING_NOT_FOUND)
+class CrowdfundingNotFoundException: IndiStrawCrowdfundingException(ErrorCode.CROWDFUNDING_NOT_FOUND)
