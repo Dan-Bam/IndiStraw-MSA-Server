3a15ba60560e6ae1c545f1db221146ab44a0cd91
yohan <noyohanx@naver.com>
2023-06-29 17:52:20 +0900

    :recycle: :: 모델 변경

--- movie/api/models.py
+++ movie/api/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-class Auth(models.Model):
-    pass
-
 class Movie(models.Model):
-    # account_index = models.IntegerField()
+    account_index = models.ForeignKey("Account", on_delete=models.PROTECT)
     title = models.CharField(max_length=255)
     description = models.TextField()
     movie_url = models.URLField(default = "")
@@ -15,8 +12,11 @@ class Movie(models.Model):
     movie_highlight = models.JSONField(default='{}')
 
 class MovieHistory(models.Model):
+    account_index = models.ForeignKey("Account", on_delete=models.CASCADE)
     movie_idx = models.ForeignKey("Movie", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    account_index = models.IntegerField()
     image_url = models.URLField(default = "")
     history_time = models.DecimalField(max_digits = 5, decimal_places = 3)
+
+class Account(models.Model):
+    account_idx = models.IntegerField()
