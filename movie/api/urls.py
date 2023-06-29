e9e81803dd931b8883b4388f493bc87cde98e71d
yohan <noyohanx@naver.com>
2023-06-29 21:01:41 +0900

    :sparkles: :: list 추가

--- movie/api/urls.py
+++ movie/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from .views import MovieViewSet, MovieDefailView
+from .views import MovieViewSet, MovieDefailView, MovieHistoryViewSet, AccountViewSet
 from rest_framework.routers import DefaultRouter
-
 router = DefaultRouter()
 router.register('', MovieViewSet)  
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('<int:pk>/', MovieDefailView.as_view(), name = "movie_api_detail")
+    path('account/', AccountViewSet.as_view({'post':'create'}), name = "account"),
+    path('movie/detail/', include(router.urls)),
+    path('movie/detail/<int:pk>/', MovieDefailView.as_view(), name = "movie_api_detail"),
+    path('movie/history/', MovieHistoryViewSet.as_view({'get': 'list','post' : 'create'}), name = "movie_history"),
+    path('movie/history/<int:account_index>/', MovieHistoryViewSet.as_view({'get': 'list'}), name = "movie_history_detail")
 ]
