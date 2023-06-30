76e3f9777d176fcafb4e00f0901f3be342d0ac60
yohan <noyohanx@naver.com>
2023-06-30 14:28:53 +0900

    :sparkles: :: actor endpoint 생성

--- movie/api/urls.py
+++ movie/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import MovieViewSet, MovieDefailView, MovieHistoryViewSet, AccountViewSet
+from .views import MovieViewSet, MovieDefailView, MovieHistoryViewSet, AccountViewSet, ActorViewSet
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('', MovieViewSet)  
@@ -8,5 +8,10 @@ urlpatterns = [
     path('account/', AccountViewSet.as_view({'post':'create'}), name = "account"),
     path('movie/detail/', include(router.urls)),
     path('movie/detail/<int:pk>/', MovieDefailView.as_view(), name = "movie_api_detail"),
-    path('movie/history/<int:account_index>/', MovieHistoryViewSet.as_view({'get': 'list','post' : 'create'}), name = "movie_history"),
+    path('movie/history/<int:account_index>/', MovieHistoryViewSet.as_view({'get': 'list',
+                                                                            'post' : 'create',
+                                                                            'put' : 'update',
+                                                                            'delete' : 'destroy'}), name = "movie_history"),
+    path('movie/actor/', ActorViewSet.as_view({'get':'list',
+                                                'post': 'create'}), name = "actor")
 ]
