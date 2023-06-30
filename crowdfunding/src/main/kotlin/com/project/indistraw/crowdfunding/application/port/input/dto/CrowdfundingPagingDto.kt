c6e8b93a9fada239ba9f9382193bdd492bce0ba2
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-06-30 11:49:10 +0900

    :fire: :: pageSize 필드 삭제

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/port/input/dto/CrowdfundingPagingDto.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/port/input/dto/CrowdfundingPagingDto.kt
@@ -1,7 +1,6 @@
 package com.project.indistraw.crowdfunding.application.port.input.dto
 
 data class CrowdfundingPagingDto(
-    val pageSize: Int,
     val isLast: Boolean,
     val list: List<CrowdfundingListDto>
 )
