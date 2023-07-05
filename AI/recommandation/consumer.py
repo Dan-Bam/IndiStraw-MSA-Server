c262e961a460f221b2787357fe4122ebd1eddeef
dong002 <81304010+dong002@users.noreply.github.com>
2023-07-05 23:19:03 +0900

    :sparkles:consumer파일 작성

--- AI/recommandation/consumer.py
+++ AI/recommandation/consumer.py
@@ -1,6 +1,6 @@
 import pika
 import json
-from main import Shop, Order, db
+from models import View_Record, Genre_Data
 
 params = pika.URLParameters('amqps://igqylvwy:TcwMgVG-nqWB4Riz7lSMPp17hEg3qOAC@vulture.rmq.cloudamqp.com/igqylvwy')
 
@@ -8,46 +8,49 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='boss')
+channel.queue_declare(queue='AI')
+
 
 def callback(ch, method, properties, body):
     print('Received in boss')
     data = json.loads(body)
     print(data)
+    if properties.content_type == 'Create_Record':
+        record_arr = data["record"]
+        dong = list(map(lambda x: x["id"], record_arr))
+        view = View_Record(account_id=data['account_id'],record=json.dumps(dong))
+        view.save()
 
-    if properties.content_type == 'shop_created':
-        shop = Shop(id=data['id'], shop_name=data['shop_name'], shop_address=data['shop_address'])
-        db.session.add(shop)
-        db.session.commit()
+    elif properties.content_type == 'Update_Record':
+        record_arr = data["record"]
+        dong = list(map(lambda x: x["id"], record_arr))
+        view = View_Record.objects.get(account_id=data['account_id'])
+        view.record = json.dumps(dong)
+        view.save()
 
-    elif properties.content_type == 'shop_updated':
-        shop = Shop.query.get(data['id'])
-        shop.shop_name = data['shop_name']
-        shop.shop_address = data['shop_address']
-        db.session.commit()
+    elif properties.content_type == 'Delete_Record':
+        view = View_Record.objects.get(account_id=data['account_id'])
+        view.delete()
 
-    elif properties.content_type == 'shop_deleted':
-        shop = Shop.query.get(data)
-        db.session.delete(shop)
-        db.session.commit()
+    elif properties.content_type == 'Create_Movie':
+        genre_arr = data["genre"]
+        dong = list(map(lambda x: x["id"], genre_arr))
+        movie = Genre_Data(movie_id=data['movie_id'], genre=json.dumps(dong))
+        movie.save()
 
-    elif properties.content_type == 'order_created':
-        order = Order(id=data['id'], shop=data['shop'], address=data['address'])
-        db.session.add(order)
-        db.session.commit()
+    elif properties.content_type == 'Update_Movie':
+        genre_arr = data["genre"]
+        dong = list(map(lambda x: x["id"], genre_arr))
+        movie = Genre_Data.objects.get(movie_id=data['movie_id'])
+        movie.genre = json.dumps(dong)
+        movie.save()
 
-    elif properties.content_type == 'order_updated':
-        order = Order.query.get(data['id'])
-        order.shop = data['shop']
-        order.address = data['address']
-        db.session.commit()
+    elif properties.content_type == 'Delete_Movie':
+        movie = Genre_Data.objects.get(movie_id=data['movie_id'])
+        movie.delete()
 
-    elif properties.content_type == 'order_deleted':
-        order = Order.query.get(data)
-        db.session.delete(order)
-        db.session.commit()
 
-channel.basic_consume(queue='boss', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='AI', on_message_callback=callback, auto_ack=True)
 
 print('Started consuming')
 
--- search/api/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Search(models.Model):
-    title = models.CharField(max_length=255)
-    genre = models.CharField(max_length=255)
