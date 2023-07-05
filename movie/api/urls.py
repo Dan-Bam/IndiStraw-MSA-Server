0c889afb5ec6dab1bbaf6e901f35b232cf9d5cb3
yohan <noyohanx@naver.com>
2023-07-05 17:45:12 +0900

    :recycle: :: 구조 변경

--- movie/api/urls.py
+++ movie/api/urls.py
@@ -6,17 +6,16 @@ router.register('', MovieViewSet)
 
 urlpatterns = [
     path('account/', AccountViewSet.as_view({'post':'create'}), name = "account"),
-    path('movie/detail/', include(router.urls)),
-    path('movie/detail/<int:pk>/', MovieDefailView.as_view(), name = "movie_api_detail"),
-    path('movie/history/<int:account_index>/', MovieHistoryViewSet.as_view({'get': 'list',
-                                                                            'post' : 'create',
-                                                                            'put' : 'update',
-                                                                            'delete' : 'destroy'}), name = "movie_history"),
+    path('movie/history/', MovieHistoryViewSet.as_view({'get': 'list',
+                                                        'post' : 'create',
+                                                        'put' : 'update',
+                                                        'delete' : 'destroy'}), name = "movie_history"),
     path('movie/actor/', ActorViewSet.as_view({'get':'list',
                                                 'post': 'create'}), name = "actor"),
     path('movie/actor/<int:pk>/', ActorDefailView.as_view(), name = "actor_detail"),
     path('movie/director/', DirectorViewSet.as_view({'get':'list',
                                                 'post': 'create'}), name = "director"),
     path('movie/director/<int:pk>/', DirectorDefailView.as_view(), name = "director_detail"),
-    
+    path('movie/<int:pk>/', MovieDefailView.as_view(), name = "movie_api_detail"),
+    path('movie/', include(router.urls)),
 ]
