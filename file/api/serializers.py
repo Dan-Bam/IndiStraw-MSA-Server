0d9bb9621dbf4950fab2231a5eaf2404c6404958
dong002 <81304010+dong002@users.noreply.github.com>
2023-07-05 11:20:11 +0900

    :art:file sturcture edit

--- file/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import File
-
-class UploadSerializer(ModelSerializer):
-    class Meta:
-        model = File
-        fields = ['file']
--- file/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import FileUploadAPIView
-
-
-# Wire up our API using automatic URL routing.
-urlpatterns = [
-    path('file/', FileUploadAPIView.as_view()),
-]
