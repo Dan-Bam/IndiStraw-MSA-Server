21091203b8e5fdf00c1eab861006b463b517b206
dong002 <81304010+dong002@users.noreply.github.com>
2023-07-07 00:01:35 +0900

    :recycle: consumer 데이터 포맷 수정

--- AI/consumer.py
+++ AI/consumer.py
@@ -24,37 +24,38 @@ def callback(ch, method, properties, body):
     data = json.loads(body)
     print(data)
     if properties.content_type == 'create_record':
-        raw_json = data["list"]
-        dong = list(map(lambda x: x["id"], raw_json))
-        view = View_Record(account_id=data['account_index'],record=json.dumps(dong))
-        view.save()
+        history = list(map(lambda x: x["movie_idx"], data))
+        db_data = View_Record.objects.filter(account_id=data[0]['account_index'])
+        if db_data.exists():
+            db_data.record = json.dumps(history)
+            db_data.save()
+        else:
+            view = View_Record(account_id=data[0]['account_index'],record=json.dumps(history))
+            view.save()
 
-    elif properties.content_type == 'update_record':
-        raw_json = data["list"]
-        dong = list(map(lambda x: x["id"], raw_json))
-        view = View_Record.objects.get(account_id=data['account_index'])
-        view.record = json.dumps(dong)
-        view.save()
+    # elif properties.content_type == 'update_record':
+    #     dong = list(map(lambda x: x["movie_idx"], data))
+    #     view = View_Record.objects.get(account_id=data['account_index'])
+    #     view.record = json.dumps(dong)
+    #     view.save()
 
     elif properties.content_type == 'delete_record':
-        view = View_Record.objects.get(account_id=data['account_index'])
+        view = View_Record.objects.get(account_id=data[0]['account_index'])
         view.delete()
 
     elif properties.content_type == 'create_movie':
-        genre_arr = data["list"]
-        dong = list(map(lambda x: x["id"], genre_arr))
-        movie = Genre_Data(movie_id=data['movie_idx'], genre=json.dumps(dong))
+        genre = list(map(lambda x: x["genre"], data))
+        movie = Genre_Data(movie_id=data[0]['movie_idx'], genre=json.dumps(genre))
         movie.save()
 
     elif properties.content_type == 'update_movie':
-        genre_arr = data["list"]
-        dong = list(map(lambda x: x["id"], genre_arr))
-        movie = Genre_Data.objects.get(movie_id=data['movie_idx'])
-        movie.genre = json.dumps(dong)
+        genre = list(map(lambda x: x["genre"], data))
+        movie = Genre_Data.objects.get(movie_id=data[0]['movie_idx'])
+        movie.genre = json.dumps(genre)
         movie.save()
 
     elif properties.content_type == 'delete_movie':
-        movie = Genre_Data.objects.get(movie_id=data['movie_idx'])
+        movie = Genre_Data.objects.get(movie_id=data[0]['movie_idx'])
         movie.delete()
 
 
