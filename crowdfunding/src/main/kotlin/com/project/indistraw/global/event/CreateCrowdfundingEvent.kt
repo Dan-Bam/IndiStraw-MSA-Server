6e0b164f906fa8502634d360ccd386c0b9cb138b
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-06 13:54:11 +0900

    :sparkles: :: rabbitMQ 세팅 추가

---
+++ crowdfunding/src/main/kotlin/com/project/indistraw/global/event/CreateCrowdfundingEvent.kt
@@ -0,0 +1,5 @@
+package com.project.indistraw.global.event
+
+class CreateCrowdfundingEvent(
+
+)
---
+++ crowdfunding/src/main/kotlin/com/project/indistraw/global/event/CreateCrowdfundingEventHandler.kt
@@ -0,0 +1,4 @@
+package com.project.indistraw.global.event
+
+class CreateCrowdfundingEventHandler {
+}
