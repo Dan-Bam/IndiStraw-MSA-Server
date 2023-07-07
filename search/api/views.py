f8a687dca96644b072ba5072366b86763197cd0b
yohan <noyohanx@naver.com>
2023-07-07 10:30:20 +0900

    :recycle: :: 페이징 적용

--- search/api/views.py
+++ search/api/views.py
@@ -2,19 +2,21 @@ from rest_framework import viewsets
 from .serializers import SearchSeriaizer
 from .models import Search
 import itertools 
+from .pagination import PageNumberPagination
 
 class SearchViewSet(viewsets.ModelViewSet):
     queryset = Search.objects.all()
     serializer_class = SearchSeriaizer        
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         qs= Search.objects.all()
         qs2 = Search.objects.all()
 
-        search_field = self.request.query_params.get('search_field')
+        search_field = self.request.query_params.get('title')
 
         if search_field is not None:
             qs = qs.filter(title__icontains=search_field)
             qs2 = qs2.filter(genre__icontains=search_field)
-            
+
         return list(itertools.chain(qs, qs2))
