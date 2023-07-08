2838b5ddd9d9b7494c7902a7359f29541c765072
yohan <noyohanx@naver.com>
2023-07-08 13:24:55 +0900

    :recycle: :: model 정리

--- search/api/models.py
+++ search/api/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 class Search(models.Model):
     title = models.CharField(max_length=255)
-    genre = models.CharField(max_length=255)
+    genre = models.JSONField()
+    view_count = models.IntegerField(default=0)
     
+class Genre(models.Model):
+    keyword = models.CharField(max_length=255)
+    view_count = models.IntegerField(default=0)
