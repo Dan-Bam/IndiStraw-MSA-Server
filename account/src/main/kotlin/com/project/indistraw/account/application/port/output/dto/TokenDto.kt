3db882794fc53e933e5f219b2d58ea1c79c298e6
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-08 20:30:11 +0900

    :sparkles: :: tokenDto toString 메서드 추가

--- account/src/main/kotlin/com/project/indistraw/account/application/port/output/dto/TokenDto.kt
+++ account/src/main/kotlin/com/project/indistraw/account/application/port/output/dto/TokenDto.kt
@@ -1,8 +1,21 @@
 package com.project.indistraw.account.application.port.output.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.time.LocalDateTime
+
 data class TokenDto(
     val accessToken: String,
     val refreshToken: String,
     val accessTokenExpiredAt: Long,
     val refreshTokenExpiredAt: Long
-)
+) {
+
+    override fun toString(): String {
+        return "{" +
+                "\"accessToken\":" + "\"" + this.accessToken + "\"," +
+                "\"refreshToken\":" + "\"" + this.refreshToken + "\"," +
+                "\"accessTokenExpiredAt\":" + LocalDateTime.now().plusSeconds(this.accessTokenExpiredAt).withNano(0) + "," +
+                "\"refreshTokenExpiredAt\":" + LocalDateTime.now().plusSeconds(this.refreshTokenExpiredAt).withNano(0) + "}"
+    }
+
+}
