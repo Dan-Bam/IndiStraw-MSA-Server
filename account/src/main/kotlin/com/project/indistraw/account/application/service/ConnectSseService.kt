cdc09f6642541be8fac93ab08cae6aea1aaa0746
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-08 11:06:09 +0900

    :sparkles: :: uuid 유효한지 검증 로직 추가

--- account/src/main/kotlin/com/project/indistraw/account/application/service/ConnectSseService.kt
+++ account/src/main/kotlin/com/project/indistraw/account/application/service/ConnectSseService.kt
@@ -1,19 +1,25 @@
 package com.project.indistraw.account.application.service
 
+import com.project.indistraw.account.application.exception.InvalidQRCodeUUIDException
 import com.project.indistraw.account.application.port.input.ConnectSseUseCase
-import com.project.indistraw.account.application.port.output.RedisPubSubPort
+import com.project.indistraw.account.application.port.output.QueryQRCodeUUIDPort
+import com.project.indistraw.account.application.port.output.SseEmitterPort
 import org.springframework.stereotype.Service
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import java.util.UUID
 
 @Service
 class ConnectSseService(
-    private val redisPubSubPort: RedisPubSubPort
+    private val sseEmitterPort: SseEmitterPort,
+    private val queryQRCodeUUIDPort: QueryQRCodeUUIDPort
 ): ConnectSseUseCase {
 
     override fun execute(uuid: UUID): SseEmitter {
+        if (queryQRCodeUUIDPort.existByUUID(uuid)) {
+            throw InvalidQRCodeUUIDException()
+        }
         // 생성된 uuid를 key로 redis sse 채널을 생성한다.
-        return redisPubSubPort.createSseToRedisChanel(uuid)
+        return sseEmitterPort.createSse(uuid)
     }
 
 }
