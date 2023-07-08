1ae36fcd1bd84c73e823e5cd4bb198a546ecf61f
dong002 <81304010+dong002@users.noreply.github.com>
2023-07-08 17:13:42 +0900

    :bug: db 에러수정

--- AI/recommandation/views.py
+++ AI/recommandation/views.py
@@ -4,21 +4,38 @@ from .models import View_Record, Genre_Data, Default_Recommandation, Recommandat
 from rest_framework import viewsets
 from .serializers import ViewSerializer
 from rest_framework.response import Response
+import json
+
+
 class ViewSet(viewsets.ModelViewSet):
     queryset = View_Record.objects.all()
     serializer_class = ViewSerializer
+
     def get_queryset(self):
+        # view = View_Record.objects.all()
+        # genre = Genre_Data.objects.all()
+        # d = {}
+        # result = []
+        # for i in range(genre.count()):
+        #     d[i] = view.filter(record__contains=i).count()
+        # #hihi = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+        # for i in range(3):
+        #     result.append(max(d,key=d.get))
+        #
+        data = [
+            {'account_index': 19, 'movie_idx': 1}
+        ]
+        history = list(map(lambda x: x["movie_idx"], data))
+        try:
+            db_data = View_Record.objects.get(account_id=data[0]['account_index'])
+        except:
+            view = View_Record(account_id=data[0]['account_index'], record=json.dumps(history))
+            view.save()
+        else:
+            db_data.record = json.dumps(history)
+            db_data.save()
+
         view = View_Record.objects.all()
-        genre = Genre_Data.objects.all()
-        d = {}
-        result = []
-        for i in range(genre.count()):
-            d[i] = view.filter(record__contains=i).count()
-        #hihi = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
-        for i in range(3):
-            result.append(max(d,key=d.get))
-
-
         return list(view)
 
 # Create your views here.
