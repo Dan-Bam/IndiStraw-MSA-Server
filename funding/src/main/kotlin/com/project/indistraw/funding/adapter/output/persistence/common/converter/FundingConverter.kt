85eb0c2f20377c9cb5e5381f35f4146e769a43ee
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-10 12:03:28 +0900

    :sparkles: :: funding converter 추가

---
+++ funding/src/main/kotlin/com/project/indistraw/funding/adapter/output/persistence/common/converter/FundingConverter.kt
@@ -0,0 +1,37 @@
+package com.project.indistraw.funding.adapter.output.persistence.common.converter
+
+import com.project.indistraw.funding.adapter.output.persistence.common.GenericConverter
+import com.project.indistraw.funding.adapter.output.persistence.entity.FundingEntity
+import com.project.indistraw.funding.domain.Funding
+import org.springframework.stereotype.Component
+
+@Component
+class FundingConverter: GenericConverter<Funding, FundingEntity> {
+
+    override fun toEntity(domain: Funding): FundingEntity =
+        domain.let {
+            FundingEntity(
+                idx = it.idx,
+                crowdfundingIdx = it.crowdfundingIdx,
+                rewordIdx = it.rewordIdx,
+                ordererIdx = it.ordererIdx,
+                price = it.price,
+                extraPrice = it.extraPrice,
+                activity = it.activity
+            )
+        }
+
+    override fun toDomain(entity: FundingEntity?): Funding? =
+        entity?.let {
+            Funding(
+                idx = it.idx,
+                crowdfundingIdx = it.crowdfundingIdx,
+                rewordIdx = it.rewordIdx,
+                ordererIdx = it.ordererIdx,
+                price = it.price,
+                extraPrice = it.extraPrice,
+                activity = it.activity
+            )
+        }
+
+}
