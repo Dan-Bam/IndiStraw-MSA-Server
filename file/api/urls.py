9e10e32bd038e3749a733b8fa7a27d3dd6000007
yohan <noyohanx@naver.com>
2023-07-10 10:10:13 +0900

    :recycle: :: dodckerfile 변경:

--- file/api/urls.py
+++ file/api/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import FileUploadAPIView
 
-
-# Wire up our API using automatic URL routing.
 urlpatterns = [
     path('file/', FileUploadAPIView.as_view(), name="file_upload"),
 ]
