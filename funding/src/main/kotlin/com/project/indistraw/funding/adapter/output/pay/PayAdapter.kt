4040e183c9a6d28d3bef7fb488c83c38de58b1f5
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-12 01:19:34 +0900

    :sparkles: :: 부트페이 결제 취소 메서드 추가

--- funding/src/main/kotlin/com/project/indistraw/funding/adapter/output/pay/PayAdapter.kt
+++ funding/src/main/kotlin/com/project/indistraw/funding/adapter/output/pay/PayAdapter.kt
@@ -1,9 +1,11 @@
 package com.project.indistraw.funding.adapter.output.pay
 
-import com.project.indistraw.funding.adapter.output.pay.exception.PayConfirmFailedException
+import com.project.indistraw.funding.adapter.output.pay.exception.FailedPayCancelException
+import com.project.indistraw.funding.adapter.output.pay.exception.FailedPayConfirmException
 import com.project.indistraw.funding.adapter.output.pay.property.BootPayProperties
 import com.project.indistraw.funding.application.port.output.PayPort
 import kr.co.bootpay.Bootpay
+import kr.co.bootpay.model.request.Cancel
 import mu.KotlinLogging
 import org.springframework.stereotype.Component
 
@@ -18,19 +20,35 @@ class PayAdapter(
         val bootpay = Bootpay(bootPayProperties.restApplicationId, bootPayProperties.privateKey)
         // bootpay 연동 전 accessToken 발급
         bootpay.accessToken
-        val confirm = bootpay.confirm(receiptId)
-
-        if (confirm["error_code"] != null) {
+        val res = bootpay.confirm(receiptId)
+        if (res["error_code"] != null) {
             log.error("receiptId confirm failed $receiptId")
-            log.error("boot pay confirm failed message is ${confirm["message"]}")
-            throw PayConfirmFailedException()
+            log.error("bootpay confirm failed message is ${res["message"]}")
+            throw FailedPayConfirmException()
         }
 
         log.info("receiptId confirm success $receiptId")
     }
 
     override fun cancel(receiptId: String) {
-        
+        val bootpay = Bootpay(bootPayProperties.restApplicationId, bootPayProperties.privateKey)
+        // bootpay 연동 전 accessToken 발급
+        bootpay.accessToken
+        val cancel = Cancel()
+        cancel.let {
+            it.receiptId = receiptId
+            it.cancelUsername = ""
+            it.cancelMessage = "test"
+        }
+
+        val res = bootpay.receiptCancel(cancel)
+        if (res["error_code"] != null) {
+            log.error("receiptId cancel failed $receiptId")
+            log.error("bootpay cancel failed message is ${res["message"]}")
+            throw FailedPayCancelException()
+        }
+
+        log.info("receiptId cancel success $receiptId")
     }
 
 }
