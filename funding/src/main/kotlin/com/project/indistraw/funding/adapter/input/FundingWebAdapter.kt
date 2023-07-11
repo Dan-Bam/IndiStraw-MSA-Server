099330b2106171ef2e33b28a6d4fb9fd8a9994e7
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-12 01:22:13 +0900

    :sparkles: :: 결제 취소 기능 추가

--- funding/src/main/kotlin/com/project/indistraw/funding/adapter/input/FundingWebAdapter.kt
+++ funding/src/main/kotlin/com/project/indistraw/funding/adapter/input/FundingWebAdapter.kt
@@ -2,6 +2,7 @@ package com.project.indistraw.funding.adapter.input
 
 import com.project.indistraw.funding.adapter.input.converter.FundingDataConverter
 import com.project.indistraw.funding.adapter.input.data.request.SaveFundingRequest
+import com.project.indistraw.funding.application.port.input.CancelFundingUseCase
 import com.project.indistraw.funding.application.port.input.SaveFundingUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -15,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/v1/funding")
 class FundingWebAdapter(
     private val fundingDataConverter: FundingDataConverter,
-    private val saveFundingUseCase: SaveFundingUseCase
+    private val saveFundingUseCase: SaveFundingUseCase,
+    private val cancelFundingUseCase: CancelFundingUseCase
 ) {
 
     @PostMapping("crowdfunding/{crowdfundingIdx}/reword/{rewordIdx}")
@@ -25,6 +27,11 @@ class FundingWebAdapter(
         @RequestBody request: SaveFundingRequest,
     ): ResponseEntity<Void> =
         saveFundingUseCase.execute(fundingDataConverter.toDto(crowdfundingIdx, rewordIdx, request))
+            .let { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @PostMapping("{idx}/receipt/{receiptId}")
+    fun cancelFunding(@PathVariable idx: Long, @PathVariable receiptId: String): ResponseEntity<Void> =
+        cancelFundingUseCase.execute(idx, receiptId)
             .let { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
 
 }
