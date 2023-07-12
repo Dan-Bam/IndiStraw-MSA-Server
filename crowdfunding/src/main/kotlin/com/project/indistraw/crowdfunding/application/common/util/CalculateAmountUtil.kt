822fe53b5a8bcf10982c23e1bd1ef56f597f1125
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-12 23:03:24 +0900

    :sparkles: :: 애그리거트 root 클래스에 애그리거트 합치기

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/common/util/CalculateAmountUtil.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/common/util/CalculateAmountUtil.kt
@@ -1,12 +1,12 @@
 package com.project.indistraw.crowdfunding.application.common.util
 
-import com.project.indistraw.crowdfunding.domain.Amount
+import com.project.indistraw.crowdfunding.domain.Crowdfunding
 import org.springframework.stereotype.Component
 
 @Component
 class CalculateAmountUtil {
 
-    fun calculateAmountPercentage(amount: Amount): Double {
+    fun calculateAmountPercentage(amount: Crowdfunding.Amount): Double {
         return if (amount.totalAmount == 0L) {
             0.0
         } else {
