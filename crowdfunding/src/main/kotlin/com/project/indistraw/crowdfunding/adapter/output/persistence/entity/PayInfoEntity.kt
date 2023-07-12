8a9d5868d60a36d22f38e1ff63bdc4975fe72c70
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-13 00:54:53 +0900

    :bug: :: id 어노테이션 이슈 해결

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/output/persistence/entity/PayInfoEntity.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/output/persistence/entity/PayInfoEntity.kt
@@ -1,14 +1,13 @@
 package com.project.indistraw.crowdfunding.adapter.output.persistence.entity
 
-import com.project.indistraw.crowdfunding.adapter.output.persistence.common.entity.BaseTimeEntity
+import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import java.util.*
-import javax.persistence.Id
 
-@RedisHash
+@RedisHash("pay_info")
 data class PayInfoEntity(
     @Id
     val id: String,
 
     val accountIdx: UUID
-): BaseTimeEntity()
+)
