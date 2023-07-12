c54d18a0dbf518b0ce87add7eeda5e809589f2ca
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-13 01:46:30 +0900

    :sparkles: :: fundingNotFound 예외처리 추가

---
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/exception/FundingNotFoundException.kt
@@ -0,0 +1,6 @@
+package com.project.indistraw.crowdfunding.application.exception
+
+import com.project.indistraw.crowdfunding.application.common.error.ErrorCode
+import com.project.indistraw.crowdfunding.application.common.error.exception.IndiStrawCrowdfundingException
+
+class FundingNotFoundException: IndiStrawCrowdfundingException(ErrorCode.FUNDING_NOT_FOUND)
