c430b3cf21167c6b51adbbb2f1242cf47e31dd69
yohan <noyohanx@naver.com>
2023-07-12 23:11:19 +0900

    :sparkles; :: account쪽 consumer 완료:

--- movie/consumer.py
+++ movie/consumer.py
@@ -7,9 +7,9 @@ import django
 
 django.setup()
 
-from api.models import Account
+from api.models import Account 
 
-params = pika.URLParameters('amqps://igqylvwy:TcwMgVG-nqWB4Riz7lSMPp17hEg3qOAC@vulture.rmq.cloudamqp.com/igqylvwy')
+params = pika.URLParameters('amqp://danbam0907:danbam1234!@svc.sel4.cloudtype.app:31704/')
 
 connection = pika.BlockingConnection(params)
 
@@ -19,14 +19,23 @@ channel.queue_declare(queue='movie')
 
 def callback(ch, method, properties, body):
     data = json.loads(body)
-    print(data)
     
     if properties.content_type == 'create_account':
-        account = Account.query.get(data['id'])
-        account.account_idx = data['account_idx']
-        account.save()
         print('Account idx has been saved.')
+        create_account_data = data.get('accountIdx')
+        account_add_data = Account(account_idx=create_account_data)
+        account_add_data.save()
 
+    elif properties.content_type == 'delete_account':
+        print('delete account')
+        create_account_data = data.get('accountIdx')
+        account_delete_data = Account.objects.get(account_idx=create_account_data)
+        account_delete_data.delete()
+
+print('Account idx has been saved.')
+
+channel.exchange_declare(exchange='direct')
+channel.queue_bind(exchange='direct', queue='movie', routing_key='create_account')
 channel.basic_consume(queue='movie', on_message_callback=callback, auto_ack=True)
 
 print('Started consuming')
