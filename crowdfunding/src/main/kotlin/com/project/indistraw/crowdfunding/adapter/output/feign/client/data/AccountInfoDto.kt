985bfe54b755980092e6e364b200712da975c14e
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-13 14:51:44 +0900

    :fire: :: feignClient 삭제

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/output/feign/client/data/AccountInfoDto.kt
+++
@@ -1,9 +0,0 @@
-package com.project.indistraw.crowdfunding.adapter.output.feign.client.data
-
-import java.util.UUID
-
-data class AccountInfoDto(
-    val accountIdx: UUID,
-    val name: String,
-    val profileUrl: String?
-)
--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/output/feign/error/exception/FeignUnAuthorizedException.kt
+++
@@ -1,6 +0,0 @@
-package com.project.indistraw.crowdfunding.adapter.output.feign.error.exception
-
-import com.project.indistraw.crowdfunding.application.common.error.ErrorCode
-import com.project.indistraw.crowdfunding.application.common.error.exception.IndiStrawCrowdfundingException
-
-class FeignUnAuthorizedException: IndiStrawCrowdfundingException(ErrorCode.FEIGN_UNAUTHORIZED)
