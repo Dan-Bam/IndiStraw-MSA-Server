7a83fac91b38706d0228b96b3e991f886de9a6ad
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-14 02:32:01 +0900

    :fire: :: receiptId 검증 로직 삭제

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/service/SaveFundingService.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/application/service/SaveFundingService.kt
@@ -1,7 +1,6 @@
 package com.project.indistraw.crowdfunding.application.service
 
 import com.project.indistraw.crowdfunding.application.common.annotation.ServiceWithTransaction
-import com.project.indistraw.crowdfunding.application.exception.ReceiptIdNotFoundException
 import com.project.indistraw.crowdfunding.application.port.input.SaveFundingUseCase
 import com.project.indistraw.crowdfunding.application.port.input.dto.SaveFundingDto
 import com.project.indistraw.crowdfunding.application.port.output.*
@@ -11,14 +10,10 @@ import com.project.indistraw.crowdfunding.domain.Funding
 class SaveFundingService(
     private val commandFundingPort: CommandFundingPort,
     private val accountSecurityPort: AccountSecurityPort,
-    private val queryPayInfoPort: QueryPayInfoPort,
     private val payPort: PayPort
 ): SaveFundingUseCase {
 
     override fun execute(dto: SaveFundingDto) {
-        if (!queryPayInfoPort.existByReceiptId(dto.receiptId)) {
-            throw ReceiptIdNotFoundException()
-        }
         // receiptId를 가지고 bootpay 결제 정보 검증 요청을 합니다.
         payPort.confirm(dto.receiptId)
 
