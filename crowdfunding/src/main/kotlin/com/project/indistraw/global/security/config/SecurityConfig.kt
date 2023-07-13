f85df80f60470c83410bf09f4c37ccf27ed59ee8
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-13 20:05:33 +0900

    :recycle: :: security 설정 메서드로 변경

--- crowdfunding/src/main/kotlin/com/project/indistraw/global/security/config/SecurityConfig.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/global/security/config/SecurityConfig.kt
@@ -3,7 +3,6 @@ package com.project.indistraw.global.security.config
 import com.project.indistraw.global.security.handler.CustomAccessDeniedHandler
 import com.project.indistraw.global.security.handler.CustomAuthenticationEntryPoint
 import com.project.indistraw.global.security.token.TokenParser
-import com.project.indistraw.global.security.token.common.emums.Authority
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -18,40 +17,31 @@ class SecurityConfig(
 ) {
 
     @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain =
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .cors()
             .and()
             .csrf().disable()
             .formLogin().disable()
             .httpBasic().disable()
-
-            .authorizeRequests()
-            // /crowdfunding
-            .mvcMatchers(HttpMethod.POST, "/api/v1/crowdfunding").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ADMIN.name)
-            .mvcMatchers(HttpMethod.GET, "/api/v1/crowdfunding/{idx}").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ADMIN.name)
-            .mvcMatchers(HttpMethod.GET, "/api/v1/crowdfunding/list").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ADMIN.name)
-            .mvcMatchers(HttpMethod.GET, "/api/v1/crowdfunding/popular/list").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ADMIN.name)
-            .mvcMatchers(HttpMethod.PATCH, "/api/v1/crowdfunding/{idx}").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ADMIN.name)
-            .mvcMatchers(HttpMethod.DELETE, "/api/v1/crowdfunding/{idx}").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ADMIN.name)
-            .mvcMatchers(HttpMethod.HEAD, "/api/v1/crowdfunding/{idx}/attend").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ADMIN.name)
-            .mvcMatchers(HttpMethod.GET, "/api/v1/crowdfunding/{idx}/users").hasAnyAuthority(Authority.ROLE_ACCOUNT.name, Authority.ROLE_ADMIN.name)
-
-            // /health
-            .mvcMatchers(HttpMethod.GET, "/").permitAll()
-
-            .anyRequest().authenticated()
-            .and()
-
-            .exceptionHandling()
-            .accessDeniedHandler(CustomAccessDeniedHandler())
-            .and()
-            .httpBasic()
-            .authenticationEntryPoint(CustomAuthenticationEntryPoint())
-            .and()
-
             .apply(FilterConfig(tokenParser))
-            .and()
-            .build()
+        authorizeHttpRequests(http)
+        exceptionHandling(http)
+        return http.build()
+    }
+
+
+    private fun authorizeHttpRequests(http: HttpSecurity) {
+        http.authorizeHttpRequests()
+            .mvcMatchers("/api/v1/crowdfunding/**").permitAll()
+            .mvcMatchers(HttpMethod.GET, "/").permitAll()
+            .anyRequest().permitAll()
+    }
+
+    private fun exceptionHandling(http: HttpSecurity) {
+        http.exceptionHandling()
+            .authenticationEntryPoint(CustomAuthenticationEntryPoint())
+            .accessDeniedHandler(CustomAccessDeniedHandler())
+    }
 
 }
