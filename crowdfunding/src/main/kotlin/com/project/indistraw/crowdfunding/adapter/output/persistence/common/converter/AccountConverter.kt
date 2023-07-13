cdc7c9b14f552f755d7c8c678b1bdeccb5541335
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-07-13 16:38:16 +0900

    :sparkles: :: TODO 삭제

--- crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/output/persistence/common/converter/AccountConverter.kt
+++ crowdfunding/src/main/kotlin/com/project/indistraw/crowdfunding/adapter/output/persistence/common/converter/AccountConverter.kt
@@ -1,17 +1,19 @@
 package com.project.indistraw.crowdfunding.adapter.output.persistence.common.converter
 
+import com.project.indistraw.crowdfunding.adapter.output.persistence.common.GenericConverter
 import com.project.indistraw.crowdfunding.adapter.output.persistence.entity.AccountEntity
 import com.project.indistraw.crowdfunding.adapter.output.persistence.entity.AddressEntity
 import com.project.indistraw.crowdfunding.domain.Account
 import org.springframework.stereotype.Component
 
 @Component
-class AccountConverter {
+class AccountConverter : GenericConverter<Account, AccountEntity> {
 
-    fun toEntity(domain: Account): AccountEntity =
+    override fun toEntity(domain: Account): AccountEntity =
         domain.let {
             AccountEntity(
                 accountIdx = it.accountIdx,
+                id = it.id,
                 name = it.name,
                 phoneNumber = it.phoneNumber,
                 address = AddressEntity(
@@ -23,4 +25,19 @@ class AccountConverter {
             )
         }
 
+    override fun toDomain(entity: AccountEntity?): Account? =
+        entity?.let {
+            Account(
+                accountIdx = it.accountIdx,
+                id = it.id,
+                name = it.name,
+                phoneNumber = it.phoneNumber,
+                address = Account.Address(
+                    zipcode = it.address?.zipcode,
+                    streetAddress = it.address?.streetAddress,
+                    detailAddress = it.address?.detailAddress
+                ),
+                profileUrl = it.profileUrl
+            )
+        }
 }
